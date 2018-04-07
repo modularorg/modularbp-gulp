2c911266297f41de54a507ef3b3c156b0d76cd91
Antoine Boulanger <hello@antoineboulanger.com>
2018-04-07 17:13:24 -0400

    Change gulp-hbs files extension from html to hbs

--- modules/gulp-hbs/src/views.js
+++ modules/gulp-hbs/src/views.js
@@ -1,19 +1,26 @@
 import gulp from 'gulp';
 import hb from 'gulp-hb';
+import rename from 'gulp-rename';
 import layouts from 'handlebars-layouts';
 import paths from './paths.js';
 import error from './error.js';
 
+const data = {
+    title: 'Title'
+}
+
 function views() {
     return gulp
-        .src(paths.views.src + '*.html')
+        .src(paths.views.src + '*.hbs')
         .pipe(hb()
-            .partials(paths.views.partials + '*.html')
+            .partials(paths.views.partials + '*.hbs')
             .helpers(layouts)
+            .data(data)
         )
         .on('error', function(err) {
             error(this, err);
         })
+        .pipe(rename({extname: '.html'}))
         .pipe(gulp.dest(paths.dest));
 }
 
